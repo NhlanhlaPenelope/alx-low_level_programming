0fbb2fb0dfe37f4a0144a86c5583f2626ec6480f
NhlanhlaPenelope <np.marule@gmail.com>
2023-08-13 13:26:11 -0700

    print alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+*
+* main - point retry
+* Return always 0 (Success)
+*/
+int main(void)
+{
+	char c;
+
+	char d;
+
+	c = 'a';
+	d = 'A';
+	while 
+		(c <= 'z') {
+			putchar (d);
+			c++;
+		}
+	while
+		(d <= 'z') {
+			putchar (d);
+			d++;
+		}
+	putchar('\n');
+	return (0);
+}
