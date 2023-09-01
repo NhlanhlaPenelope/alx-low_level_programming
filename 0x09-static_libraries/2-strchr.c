ff68148e995ca85a99ebde62cba5b155e38e0218
NhlanhlaPenelope <np.marule@gmail.com>
2023-09-01 07:25:30 -0700

    2-strchr.c

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - Entry point
+ * @s: input
+ * @c: input
+ * Return: Alawys 0 (Success)
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	for (; s[i] >= '\0', i++)
+	{
+		if (s[i] == c)
+			return (&[i];
+	}
+	return (0);
+}
