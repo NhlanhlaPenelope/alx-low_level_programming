219e63f5b1fc95e6ef486feef122dc56ecb441b6
NhlanhlaPenelope <np.marule@gmail.com>
2023-09-04 07:11:59 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -8,22 +8,22 @@
 */
 char *_strdup(char *str)
 {
-        char *aaa;
+	char *aaa;
 	int i, r = 0;
 
-	 if (str == NULL)
-                return (NULL);
+	if (str == NULL)
+		return (NULL);
 	 i = 0;
 	 while (str[i] != '\0')
-                i++;
+		 i++;
 
 	 aaa = malloc(sizeof(char) * (i + 1));
- 
-         if (aaa == NULL)
-                 return (NULL);
- 
-         for (r = 0; str[r]; r++)
-                 aaa[r] = str[r];
+
+	 if (aaa == NULL)
+		 return (NULL);
+
+	 for (r = 0; str[r]; r++)
+		 aaa[r] = str[r];
 
 	 return (aaa);
- }
+}
